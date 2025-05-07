d3f6c81ca31affa8da437b02887b4053806b7edd
Victor <victor.lian@microchip.com>
2025-05-07 11:52:44 -0500

    Updated quick_start to handle camera setup download

--- quick_start.sh
+++ quick_start.sh
@@ -6,22 +6,27 @@ if [[ $(uname -r) != $YOCTO ]]; then
 	exit
 fi
 
-#Setup camera if not found
-if [ -d release-v$VER ]; then
-	cd release-v$VER/example/soc-video-c
-	if [ ! -f /opt/microchip/auto_gain ]; then
-		bash setup_camera.sh
-	fi
-fi
-
 if [ ! -d samples_V1000_$VER ]; then
 	wget --no-check-certificate https://github.com/Microchip-Vectorblox/VectorBlox-SoC-Video-Kit-Demo/releases/download/release-v$VER/samples_V1000_$VER.zip ~
 
-	unzip samples_V1000_$VER.zip
+	unzip samples_V1000_$VER.zip -d ~
 fi
 
-if [ ! -d release-v$VER ]; then
+if [ ! -d VectorBlox-SDK-release-v$VER ]; then
 	wget --no-check-certificate https://github.com/Microchip-Vectorblox/VectorBlox-SDK/archive/refs/tags/release-v$VER.zip ~
 
-	unzip release-v$VER.zip
+	unzip release-v$VER.zip -d ~
+fi
+
+#Setup camera if not found
+if [ -d VectorBlox-SDK-release-v$VER ]; then
+	cd VectorBlox-SDK-release-v$VER/example/soc-video-c
+	if [ ! -f /opt/microchip/auto_gain ]; then
+		if [ ! -f setup_camera.sh ]; then
+			wget --no-check-certificate https://github.com/Microchip-Vectorblox/assets/releases/download/assets/camera_setup.zip
+			
+			unzip camera_setup.zip
+		fi
+		bash setup_camera.sh
+	fi
 fi
